f00522c11fbd29d11d597f25b6a819a9e25c66f0
Shouklier <tawfikcraft@gmail.com>
2025-02-07 15:44:06 +0200

    added softmax function and 'derivative' of it

--- activationFuncs.py
+++ activationFuncs.py
@@ -13,3 +13,17 @@ class Sigmoid:
     
     def derivative(self, preActivatedInput):
         return self(preActivatedInput) * (1 - self(preActivatedInput))
+
+class Softmax:
+    def __call__(self, preActivatedInput):
+        if preActivatedInput.ndim == 1:
+            preActivatedInput.reshape(1, -1)
+        exped = np.exp(preActivatedInput - np.max(preActivatedInput, axis = 1, keepdims=True))
+
+        return exped / np.sum(exped, axis = 1, keepdims = True)
+
+    def derivative(self, x):
+        # Since I use Cross Entropy Loss, MSE Already Returns Desired Derivative
+        return x / 2
+
+
