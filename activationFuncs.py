2d2717953cd19ac0e9328d67f35c94ea9877689c
Shouklier <tawfikcraft@gmail.com>
2025-02-09 18:40:46 +0200

    made sure activation functions can accept vectors or 1-dim. arrays as input

--- activationFuncs.py
+++ activationFuncs.py
@@ -2,16 +2,24 @@ import numpy as np
 
 class ReLU:
     def __call__(self, preActivatedInput):
+        if preActivatedInput.ndim == 1:
+            preActivatedInput.reshape(1, -1)
         return np.where(preActivatedInput<= 0, 0, preActivatedInput)
     
     def derivative(self, preActivatedInput):
+        if preActivatedInput.ndim == 1:
+            preActivatedInput.reshape(1, -1)
         return np.where(preActivatedInput<= 0, 0, 1)
 
 class Sigmoid:
     def __call__(self, preActivatedInput):
+        if preActivatedInput.ndim == 1:
+            preActivatedInput.reshape(1, -1)
         return 1 / (1 + np.exp(-preActivatedInput))
     
     def derivative(self, preActivatedInput):
+        if preActivatedInput.ndim == 1:
+            preActivatedInput.reshape(1, -1)
         return self(preActivatedInput) * (1 - self(preActivatedInput))
 
 class Softmax:
@@ -24,6 +32,8 @@ class Softmax:
 
     def derivative(self, x):
         # Since I use Cross Entropy Loss, MSE Already Returns Desired Derivative
+        if x.ndim == 1:
+            x.reshape(1, -1)
         return x / 2
 
 
